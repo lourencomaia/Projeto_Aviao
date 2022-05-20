5e27f4ed8d65353e2e91f776c51a8f0f18288699
lourencomaia <lourenco.maia.2000@gmail.com>
2022-05-20 17:11:33 +0100

    inimigo/colisao/vida
    foi acrescentado um novo jogador,foi acrescentado a colisao das balas com os avioes e a vida do aviao

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ let ctx = canvas.getContext('2d');
 canvas.height=800;
 canvas.width=1700;
 
+vida1=document.getElementById("aviao1");
+vida2=document.getElementById("aviao2");
+
+
 let keys=[];
-let balaAviao=[];
+
 
 let rato={
     x:0,
@@ -22,9 +26,49 @@ class Aviao extends AnimatedSprite{
         super(sx, sy, width, height);
         this.speed=5;
         this.frameCounter = 0;
-        this.shootDelay = 120; //2s
+        this.shootDelay = 60; //2s
+        this.balaAviao=[];
+        this.vida=100;
+    }
+    update(){
+        super.update()
+
+        if (keys['w']) {
+            this.y -= this.speed;
+        }
+
+        if (keys['s']) {
+            this.y += this.speed;
+        }
+        if (this.frameCounter < this.shootDelay) {
+            this.frameCounter++;
+        }
+        if (keys[' '] && this.frameCounter === this.shootDelay) {
+            this.atirar();
+            this.frameCounter =0;
+        }
+
+    }
+    draw() {
+        ctx.drawImage(this.imagem, this.sx, this.sy, this.slice.width, this.slice.height,
+            this.x, this.y, this.width*1.2, this.height*1.2);
     }
 
+    atirar() {
+       this.balaAviao.push(new Bala(aviao1.x+aviao1.width/2,aviao1.y+aviao1.height,477/7,78/2,-10));
+       this.balaAviao[this.balaAviao.length-1].load("/assets/bala.png",13,7,1)
+    }
+}
+
+class AviaoInimigo extends AnimatedSprite{
+    constructor(sx, sy, width, height) {
+        super(sx, sy, width, height);
+        this.speed=5;
+        this.frameCounter = 0;
+        this.shootDelay = 60; //2s
+        this.balaAviao=[];
+        this.vida=100;
+    }
     update(){
         super.update()
 
@@ -38,10 +82,9 @@ class Aviao extends AnimatedSprite{
         if (this.frameCounter < this.shootDelay) {
             this.frameCounter++;
         }
-        if (keys[' '] && this.frameCounter === this.shootDelay) {
+        if (keys['q'] && this.frameCounter === this.shootDelay) {
             this.atirar();
             this.frameCounter =0;
-
         }
 
     }
@@ -51,24 +94,37 @@ class Aviao extends AnimatedSprite{
     }
 
     atirar() {
-       balaAviao.push(new Bala(aviao1.x+aviao1.width/2,aviao1.y+aviao1.height,477/7,78/2));
-       balaAviao[balaAviao.length-1].load("/assets/bala.png",13,7,1)
+        this.balaAviao.push(new Bala(aviaoInimigo.x+aviaoInimigo.width/2,aviaoInimigo.y+aviaoInimigo.height,477/7,78/2,10));
+        this.balaAviao[this.balaAviao.length-1].load("/assets/bala.png",13,7,1)
     }
 }
 
 class Bala extends AnimatedSprite{
-    constructor(sx, sy, width, height) {
+    constructor(sx, sy, width, height,speed) {
         super(sx, sy, width, height);
-        this.speed=5;
+        this.speed=speed;
     }
     update() {
         super.update();
+        this.x+=this.speed;
 
-        this.x-=10;
+        collision(this,aviao1)
+        collision(this,aviaoInimigo)
     }
     draw() {
         ctx.drawImage(this.imagem, this.sx, this.sy, this.slice.width, this.slice.height,
             this.x, this.y, this.width*0.25, this.height*0.25);
+    }
+
+}
+
+function collision(bala, aviao) {
+    if (bala.x < aviao.x + aviao.width &&
+        bala.x + bala.width > aviao.x &&
+        bala.y < aviao.y + aviao.height &&
+        bala.y + bala.height > aviao.y) {
+       console.log("acertou aviao");
+       aviao.vida--;
     }
 
 }
@@ -83,15 +139,11 @@ window.addEventListener('load',startGame);
 let fundo= new Sprite(0,0,1708,853);
 fundo.load("/assets/fundo.png");
 
-let aviao1 = new Aviao(1200,200,410/5,44);
+let aviao1 = new AviaoInimigo(1200,200,410/5,44);
 aviao1.load("/assets/aviao.png",5,5,10);
 
-
-
-//console.log(aviao1);
-
-
-
+let aviaoInimigo = new Aviao(200,200,410/5,44);
+aviaoInimigo.load("/assets/aviao1.png",5,5,10);
 
 function startGame() {
     animated();
@@ -104,18 +156,30 @@ function animated() {
 
     aviao1.update()
     aviao1.draw();
-    console.log(aviao1)
+
+    aviaoInimigo.update();
+    aviaoInimigo.draw();
+
 
     //desenhar as balas
-    for(let bala of balaAviao){
+    for(let bala of aviao1.balaAviao){
         bala.update();
         bala.draw();
     }
+    for(let bala of aviaoInimigo.balaAviao){
+        bala.update();
+        bala.draw();
+    }
+
+    vida1.innerHTML= "Aviao 1: "+ aviaoInimigo.vida;
+    vida2.innerHTML= "Aviao 2: "+ aviao1.vida;
+
+
 }
 //Eventos do teclado
 window.addEventListener('keydown', (e) => {
     keys[e.key] = true;
-    console.log(e.key)
+
 });
 
 window.addEventListener('keyup', (e) => {
