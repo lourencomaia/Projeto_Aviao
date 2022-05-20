309a00a9b6da9bcb2b36d34a360277c35e1f5d6b
lourencomaia <lourenco.maia.2000@gmail.com>
2022-05-20 16:07:13 +0100

    aviao dispara e anda
    foi adicionada a funcionalidade de andar para cima e para baixo e de disparar

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
-canvas.height=500;
-canvas.width=1000;
+canvas.height=800;
+canvas.width=1700;
+
 let keys=[];
+let balaAviao=[];
 
 let rato={
     x:0,
@@ -18,7 +20,57 @@ let rato={
 class Aviao extends AnimatedSprite{
     constructor(sx, sy, width, height) {
         super(sx, sy, width, height);
+        this.speed=5;
+        this.frameCounter = 0;
+        this.shootDelay = 120; //2s
     }
+
+    update(){
+        super.update()
+
+        if (keys['ArrowUp']) {
+            this.y -= this.speed;
+        }
+
+        if (keys['ArrowDown']) {
+            this.y += this.speed;
+        }
+        if (this.frameCounter < this.shootDelay) {
+            this.frameCounter++;
+        }
+        if (keys[' '] && this.frameCounter === this.shootDelay) {
+            this.atirar();
+            this.frameCounter =0;
+
+        }
+
+    }
+    draw() {
+        ctx.drawImage(this.imagem, this.sx, this.sy, this.slice.width, this.slice.height,
+            this.x, this.y, this.width*1.2, this.height*1.2);
+    }
+
+    atirar() {
+       balaAviao.push(new Bala(aviao1.x+aviao1.width/2,aviao1.y+aviao1.height,477/7,78/2));
+       balaAviao[balaAviao.length-1].load("/assets/bala.png",13,7,1)
+    }
+}
+
+class Bala extends AnimatedSprite{
+    constructor(sx, sy, width, height) {
+        super(sx, sy, width, height);
+        this.speed=5;
+    }
+    update() {
+        super.update();
+
+        this.x-=10;
+    }
+    draw() {
+        ctx.drawImage(this.imagem, this.sx, this.sy, this.slice.width, this.slice.height,
+            this.x, this.y, this.width*0.25, this.height*0.25);
+    }
+
 }
 
 
@@ -28,13 +80,15 @@ window.addEventListener('load',startGame);
 
 //let aviao = new Aviao(200,400,454,44);
 //aviao.load("aviao.png");
+let fundo= new Sprite(0,0,1708,853);
+fundo.load("/assets/fundo.png");
 
-let aviao1 = new Aviao(200,200,442/5,44);
-aviao1.load("pngegg.png",5,5,10);
+let aviao1 = new Aviao(1200,200,410/5,44);
+aviao1.load("/assets/aviao.png",5,5,10);
 
 
 
-console.log(aviao1);
+//console.log(aviao1);
 
 
 
@@ -43,15 +97,20 @@ function startGame() {
     animated();
 }
 function animated() {
+
     ctx.clearRect(0,0,canvas.width,canvas.height);
     requestAnimationFrame(animated);
-
-    //aviao.update()
-    //aviao.draw();
+    fundo.draw();
 
     aviao1.update()
     aviao1.draw();
     console.log(aviao1)
+
+    //desenhar as balas
+    for(let bala of balaAviao){
+        bala.update();
+        bala.draw();
+    }
 }
 //Eventos do teclado
 window.addEventListener('keydown', (e) => {
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 #projeto AM
+
+Lourenço Emanuel Gomes Maia 
+Nº190100386
+
+Ruben Luis Gomes Pedro 
+Nº190100484
